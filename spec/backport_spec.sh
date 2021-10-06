97ab382f726b0b6a7dee6bab013b1c217c3a1d30
Michael Tupitsyn <mikhail.tupitsyn@hpe.com>
2021-10-05 22:01:50 -0700

    Adjust test to reflect debug output

--- spec/backport_spec.sh
+++ spec/backport_spec.sh
@@ -203,7 +203,7 @@ http_post invoked with:comments-url {\"body\":\"Unable to cherry-pick commit * o
     It 'Pushes'
       When call push 'backport-branch'
       The value "$(cd "${git_repository}" && git show backport-branch:file)" should equal "modified"
-      The output should start with "Branch 'backport-branch' set up to track remote branch 'backport-branch' from 'origin'"
+      The output should match pattern "::debug::out: Branch 'backport-branch' set up to track remote branch 'backport-branch' from 'origin'"
     End
   End
 
